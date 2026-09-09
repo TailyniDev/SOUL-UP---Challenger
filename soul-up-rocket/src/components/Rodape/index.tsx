d7ef1b0d15389290b820805ee9757d2f8b5c6c3b
TailyniDev <tailyni80@gmail.com>
2026-09-09 20:27:48 -0300

    feat: cria componente Rodape

--- soul-up-rocket/src/components/Rodape/index.tsx
+++ soul-up-rocket/src/components/Rodape/index.tsx
@@ -1,7 +1,26 @@
 export default function Rodape() {
   return (
     <footer>
-      <p>Meu rodapé</p>
+      <div><span>🌿</span> SoulUp Rocket<p>Transformando pequenas ações em grandes mudanças.</p></div>
+      <div>
+        <h4>Links Rápidos</h4>
+        <ul>
+          <li>Início</li>
+          <li>Sobre o Projeto</li>
+          <li>FAQ</li>
+          <li>Integrantes</li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Parceria</h4>
+        <p>🤝 SoulUp + FIAP</p>
+        <p>📧 challenge@soulup.com</p>
+      </div>
+
+      <div>
+        <p>© 2026 SoulUpRocket - Challenge SoulUp | 1º Semestre</p>
+      </div>
     </footer>
   );
 }
