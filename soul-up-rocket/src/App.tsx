d42d24fdce74938fbf58143817015452960534f7
TailyniDev <tailyni80@gmail.com>
2026-09-09 19:57:58 -0300

    feat: cria componentes Cabecalho, Menu e Rodape

--- soul-up-rocket/src/App.tsx
+++ soul-up-rocket/src/App.tsx
@@ -1,7 +1,16 @@
-import { useState } from 'react'
+import Cabecalho from "./components/Cabecalho";
+import Rodape from "./components/Rodape";
 
-function App() {
-  return ( <main> <h1 >SoulUp Rocket</h1> </main> )
+export default function App() {
+  return (
+    <>
+      <Cabecalho />
+
+      <main>
+        <h2>Conteúdo da página vai aqui</h2>
+      </main>
+
+      <Rodape />
+    </>
+  );
 }
-
-export default App
---
+++ soul-up-rocket/src/components/Cabecalho/index.tsx
@@ -0,0 +1,7 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>SoulUp Rocket</h1>
+    </header>
+  );
+}
